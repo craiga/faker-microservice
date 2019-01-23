3e168c01b758d64542e707d9afb03ca5b0c4a513
Craig Anderson <craiga@craiga.id.au>
2019-01-23 15:03:14 +0000

    Adding lots more potential names.

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-from faker import Faker
+from faker import Faker, Generator
 
 import faker_microservice
 
@@ -10,11 +10,55 @@ import faker_microservice
 class IntegrationTestCase(unittest.TestCase):
     """Integration tests."""
 
-    def test_integration(self):  # pylint: disable=no-self-use
+    def setUp(self):
+        self.fake = Faker()
+        self.fake.add_provider(faker_microservice.Provider)
+
+    def test_integration(self):
         """Test integration with Faker."""
-        fake = Faker()
-        fake.add_provider(faker_microservice.Provider)
-        fake.microservice()
+        result = self.fake.microservice()
+        self.assertIsInstance(result, str)
+        self.assertGreater(len(result), 1)
+
+    def test_private_unavailable(self):
+        """Test private method isn't available through Faker."""
+        # pylint: disable=no-member,protected-access
+        with self.assertRaises(AttributeError):
+            self.fake._microservice_simple()
+        with self.assertRaises(AttributeError):
+            self.fake.microservice_simple()
+
+
+class MicroserviceProviderTestCase(unittest.TestCase):
+    """Provider test case."""
+
+    # pylint: disable=protected-access
+
+    def setUp(self):
+        self.provider = faker_microservice.Provider(Generator())
+
+    def test_simple(self):
+        """Test simple microservice names."""
+        result = self.provider._microservice_simple()
+        self.assertIsInstance(result, str)
+        self.assertGreater(len(result), 1)
+
+    def test_delimiter_and_suffix(self):
+        """Test microservice names with delimiter and suffix."""
+        result = self.provider._microservice_with_delimiter_and_suffix()
+        self.assertIsInstance(result, str)
+        self.assertGreater(len(result), 1)
+
+    def test_lists_in_order(self):
+        """Test lists in root of module are in order."""
+        for attr_name in dir(faker_microservice):
+            with self.subTest(attr_name=attr_name):
+                attr = getattr(faker_microservice, attr_name)
+                if isinstance(attr, list):
+                    prev_value = ""
+                    for this_value in attr:
+                        self.assertGreaterEqual(this_value, prev_value)
+                        prev_value = this_value
 
 
 if __name__ == "__main__":
